1b6a979895bcf6b9e0802997df00af4d0d4ffa24
Jeppe Bårris <jeppe@redpitt.com>
2015-12-13 00:12:22 +0100

    Disable jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -416,7 +416,7 @@ module.exports = function (grunt) {
     'sass:server',
     'coffeelint:check',
     'coffee:dist',
-    'jshint:all',
+    // 'jshint:all',
     'csslint:check'
     // 'scsslint'
   ]);
